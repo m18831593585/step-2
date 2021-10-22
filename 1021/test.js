c7b899f64ba77da663f2c5efacacd9154cf479e6
Caulic <im@caulic.com>
2021-10-22 13:49:18 +0800

    10/22

--- 1021/test.js
+++ 1021/test.js
@@ -56,6 +56,69 @@ function sort1(arr){
     }
     console.log(arr)
 }
-sort1(arr)
+// sort1(arr)
 // console.log(arr)
 
+
+// foreach
+// function arrayForEach(arr, fn){
+//     if(!Array.isArray(arr)) throw new Error("error")
+//     for(let i=0;i<arr.length; i++){
+//         fn(arr[i], i, arr)
+//     }
+// }
+//
+// function fn(value, index, arr){
+//     console.log(value, index)
+// }
+// arrayForEach(arr, fn)
+
+
+// map
+// function arrayMap(arr, fn){
+//     if(!Array.isArray(arr)) throw new Error("error")
+//     let newArr=[]
+//     for (let i=0;i<arr.length;i++){
+//         if(!(i in arr)) continue
+//         newArr[i] = fn(arr[i], i, arr)
+//     }
+//     return newArr
+// }
+//
+// function fn(value, index, arr){
+//     return String(value)+String(index)
+// }
+// console.log(arrayMap(arr,fn))
+
+
+// reduce
+// function arrayReduce(arr, fn, init){
+//     if(!Array.isArray(arr)) throw new Error("error")
+//     let index, result
+//     result = init===undefined ? init : arr[0]
+//     index = init===undefined ? 0 : 1
+//     for(; index<arr.length; index++){
+//         result += fn(result, arr[index], index, arr)
+//     }
+//     return result
+// }
+
+
+// quickSort
+function quickSort(arr){
+    if(!Array.isArray(arr)) throw new Error("error")
+    if(arr.length<=1) return arr
+    let left=[], right=[]
+    // let pivotIndex = ~~(arr.length / 2)
+    let pivot = Number(arr.splice(~~(arr.length / 2), 1))
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] < pivot) {
+            left.push(arr[i])
+        }else{
+            right.push(arr[i])
+        }
+    }
+    return quickSort(left).concat(pivot, quickSort(right))
+}
+
+console.log(quickSort(arr))
