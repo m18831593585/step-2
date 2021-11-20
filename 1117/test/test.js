9d7196e6b5f2b934a7d85d7b022f05152bc0f9b5
Caulic <im@caulic.com>
2021-11-20 08:36:28 +0800

    11/19

--- 1117/test/test.js
+++ 1117/test/test.js
@@ -1,2 +1,42 @@
+let o = {
+    _a: 1,
+    b: 2,
+    c: 3,
 
+    get a() {
+        return this._a;
+    },
+
+    set a(value) {
+        this._a = value;
+    }
+
+};
+
+Object.defineProperty(o, "_a", {
+    enumerable: false
+})
+
+// console.log(o)
+
+
+let f = function () {
+    let pv = 1;
+    console.log(this)
+    return function () {
+        console.log(pv)
+    }
+}
+
+
+function currying() {
+    let sum = 0
+    return function () {
+        let args = [].slice.call(arguments);
+        if (args.length === 0) console.log(sum);
+        else while (args.length) sum += args.shift()
+    }
+}
+
+currying(1, 2, 3)()
 
